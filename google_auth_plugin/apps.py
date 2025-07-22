59d71317e4f66495927f51423129f95d11189ead
Aayush Kunwar <kunwar_aayush@live.com>
2025-07-22 17:47:54 +0545

    FIX: changed plugin name in apps.py

--- google_auth_plugin/apps.py
+++ google_auth_plugin/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class GoogleAuthAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'google_auth_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "google_auth_plugin"
