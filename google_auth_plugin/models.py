41f0c92bad7cc274ad29464c55eadea577f3ae1c
Aayush Kunwar <kunwar_aayush@live.com>
2025-08-13 12:15:15 +0545

    FIX: updated the single instance check again

--- google_auth_plugin/models.py
+++ google_auth_plugin/models.py
@@ -6,12 +6,6 @@ class GoogleCredential(models.Model):
     client_secret = models.CharField(max_length=255, help_text="Google OAuth2 Client Secret")
     redirect_uri = models.URLField(max_length=255, blank=True, null=True)
 
-    def save(self, *args, **kwargs):
-        existing = GoogleCredential.objects.first()
-        if existing and not self.pk:
-            self.pk = existing.pk  # overwrite existing instead of creating new
-        super().save(*args, **kwargs)
-
     def __str__(self):
         return "Google OAuth2 Credentials"
 
